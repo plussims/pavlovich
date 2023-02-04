307ac9a34021e3f61832255061746325fc351ce6
plussims <123937911+plussims@users.noreply.github.com>
2023-02-04 08:32:52 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,7 @@
-var burgerMenu = document.querySelector('.menu__burger');
-var burgerMenuClose = document.querySelector('.menu__burger close');
-var overlay = document.querySelector('.menu__mobile .menu');
-burgerMenu.addEventListener('click',function(){
-    if (overlay.style.display === "none") {
-      overlay.style.display = "block";
-    }
-    burgerMenu.classList.toggle("close");
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu__burger');
+
+burgerMenu.addEventListener('click',function(close){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
 });
-// burgerMenuClose.addEventListener('click',function(){
-//     if (overlay.style.display === "block") {
-//       overlay.style.display = "none";
-//     }
-// });
